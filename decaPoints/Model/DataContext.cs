67ae6adbc9e441b6f4989fe216be0f6bc213d3c1
Johannes Treiel <jtreie@ttu.ee>
2022-03-02 00:24:42 +0200

    Add project files.

---
+++ decaPoints/Model/DataContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace decaPoints.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> PersonList { get; set; }
+         
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Person>().Property(p => p.ID).HasIdentityOptions(startValue: 2);
+
+            modelBuilder.Entity<Person>().HasData(
+                new Person
+                {
+                    ID = 1,
+                    Name = "Peter Pan",
+                    Gender = Gender.Male,
+                    HundredMeters = 11.1,
+                    LongJump = 7.11,
+                    ShotPut = 13.2,
+                    HighJump = 2.05,
+                    FourHundredMeters = 50.1,
+                    Hurdles = 14.2,
+                    DiscusThrow = 40.2,
+                    PoleVault = 4.80,
+                    Javelin = 59,
+                    FifteenHundredMeters = "4:32.2",
+
+                }) ;
+
+        }
+    }
+
+}
---
+++ decaPoints/Model/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace decaPoints.Model
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public double HundredMeters { get; set; }
+        public double LongJump { get; set; }
+        public double ShotPut { get; set; }
+        public double HighJump { get; set; }
+        public double FourHundredMeters { get; set; }
+        public double Hurdles { get; set; }
+        public double DiscusThrow { get; set; }
+        public double PoleVault { get; set; }
+        public double Javelin { get; set; }
+        public String FifteenHundredMeters { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+    }
+}

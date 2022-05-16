a1e27ed895f11f92687456fb556aa20ad2866646
Johannes Treiel <jtreie@ttu.ee>
2022-05-16 21:54:44 +0300

    Edit works without losing focus

---
+++ deca-points-react-js/src/components/EditPopUp.js
@@ -0,0 +1,80 @@
+
+import { Button, Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import "../App.css"
+import {  doc, updateDoc } from "firebase/firestore"
+import { db } from '../firebase';
+
+
+function EditPopUp(props) {
+    //Add deca
+ const [name, setName] = useState('')
+ const [dateOfBirth, setDOB] = useState('')
+ const [hundredMeters, setHundred] = useState('')
+ const [longJump, setLong] = useState('')
+ const [shotPut, setShot] = useState('')
+ const [highJump, setHigh] = useState('')
+ const [fourHundredMeters, setFour] = useState('')
+ const [hurdles, setHurdles] = useState('')
+ const [discus, setDisc] = useState('')
+ const [poleVault, setPole] = useState('')
+ const [javelin, setJav] = useState('')
+ const [minutes, setMin] = useState('')
+ const [seconds, setSec] = useState('')
+ const [uid, setUid] = useState('')
+ const [id, setId] = useState('')
+
+ function handleSubmitChange(e) {
+    e.preventDefault();
+    if (name === "" || id === "") {
+      return
+    }
+    const docRef = doc(db, 'decaTable', id)
+    updateDoc(docRef, {
+      id, name, dateOfBirth, hundredMeters, longJump, shotPut, highJump, fourHundredMeters,
+      hurdles, discus, poleVault, javelin, minutes, seconds,
+    }).then(response => {
+      console.log(response)
+    })
+      .catch(error => console.log(error.message))
+  }
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className='modal'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Edit
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form className='form' onSubmit={handleSubmitChange}>
+            <input placeholder='WRJFXrJhAN0UsJWw5WLV' id='id' type="text" value={id} onChange={e => setId(e.target.value)} />
+            <input placeholder='Name' id='name' type="text" value={name} onChange={e => setName(e.target.value)} />
+            <input placeholder='01/01/2010' id='dateOfBirth' type="text" value={dateOfBirth} onChange={e => setDOB(e.target.value)} />
+            <input placeholder='11.2' id='hundredMeters' type="decimal" value={hundredMeters} onChange={e => setHundred(e.target.value)} />
+            <input placeholder='6.52' id='longJump' type="decimal" value={longJump} onChange={e => setLong(e.target.value)} />
+            <input placeholder='13.30' id='shotPut' type="decimal" value={shotPut} onChange={e => setShot(e.target.value)} />
+            <input placeholder='1.94' id='highJump' type="decimal" value={highJump} onChange={e => setHigh(e.target.value)} />
+            <input placeholder='48.20' id='fourHundredMeters' type="decimal" value={fourHundredMeters} onChange={e => setFour(e.target.value)} />
+            <input placeholder='13.78' id='hurdles' type="decimal" value={hurdles} onChange={e => setHurdles(e.target.value)} />
+            <input placeholder='45.2' id='discus' type="decimal" value={discus} onChange={e => setDisc(e.target.value)} />
+            <input placeholder='4.20' id='poleVault' type="decimal" value={poleVault} onChange={e => setPole(e.target.value)} />
+            <input placeholder='56.20' id='javelin' type="decimal" value={javelin} onChange={e => setJav(e.target.value)} />
+            <input placeholder='4' id='minutes' type="decimal" value={minutes} onChange={e => setMin(e.target.value)} />
+            <input placeholder='28.2' id='seconds' type="decimal" value={seconds} onChange={e => setSec(e.target.value)} />
+            <button name='btnName' type="submit" >Update</button>
+        </form>
+        </Modal.Body>
+        <Modal.Footer>
+          
+        <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+  export default EditPopUp;
--- deca-points-react-js/src/pages/Decathlon.js
+++ deca-points-react-js/src/pages/Decathlon.js
@@ -10,13 +10,11 @@ import disc from "../Calculator/Decathlon/Throws/discusThrow.js";
 import pole from "../Calculator/Decathlon/Jumps/poleVault.js";
 import jav from "../Calculator/Decathlon/Throws/javelinThrow.js";
 import fifteenHundred from "../Calculator/Decathlon/Runs/fifteenHundredMeters.js"
-import { Table } from "react-bootstrap"
 import { collection, getDocs, addDoc, doc, deleteDoc, onSnapshot, updateDoc, query } from "firebase/firestore"
-import { ref as sRef } from "firebase/storage"
-import { orderByChild } from "firebase/database"
 import { db } from '../firebase';
 import { Button, Modal } from 'react-bootstrap';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import EditPopUp from "../components/EditPopUp"
 import { CSVLink } from 'react-csv'
 
 // import { } from "bootstrap/dist/css/bootstrap.min.css";
@@ -88,14 +86,7 @@ const Decathlon = () => {
     if (name === '') {
       return
     }
-    // const number = uid.substring(0,7) + Math.floor(Math.random()*(999-100+1)+100);
-
     const decathletesCollectionRef = collection(db, 'decaTable');
-    // setDoc(doc(decathletesCollectionRef, number),
-    //   {
-    //     name, dateOfBirth, hundredMeters, longJump, shotPut, highJump, fourHundredMeters,
-    //     hurdles, discus, poleVault, javelin, minutes, seconds
-    //   })
     addDoc(decathletesCollectionRef,
       {
         name, dateOfBirth, hundredMeters, longJump, shotPut, highJump, fourHundredMeters,
@@ -127,8 +118,6 @@ const Decathlon = () => {
 
   //Edit athletes
   const [modalShow, setModalShow] = useState(false);
-  const [tempUid, setTempUid] = useState('');
-  const [isEdit, setIsEdit] = useState(false);
 
   function handleSubmitChange(e) {
     e.preventDefault();
@@ -145,44 +134,7 @@ const Decathlon = () => {
       .catch(error => console.log(error.message))
   }
   //function for edit popup
-  function EditPopUp(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Edit
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form className='form' onSubmit={handleSubmitChange}>
-            <input placeholder='WRJFXrJhAN0UsJWw5WLV' id='id' type="text" value={id} onChange={e => setId(e.target.value)} />
-            <input placeholder='Name' id='name' type="text" value={name} onChange={e => setName(e.target.value)} />
-            <input placeholder='01/01/2010' id='dateOfBirth' type="text" value={dateOfBirth} onChange={e => setDOB(e.target.value)} />
-            <input placeholder='11.2' id='hundredMeters' type="decimal" value={hundredMeters} onChange={e => setHundred(e.target.value)} />
-            <input placeholder='6.52' id='longJump' type="decimal" value={longJump} onChange={e => setLong(e.target.value)} />
-            <input placeholder='13.30' id='shotPut' type="decimal" value={shotPut} onChange={e => setShot(e.target.value)} />
-            <input placeholder='1.94' id='highJump' type="decimal" value={highJump} onChange={e => setHigh(e.target.value)} />
-            <input placeholder='48.20' id='fourHundredMeters' type="decimal" value={fourHundredMeters} onChange={e => setFour(e.target.value)} />
-            <input placeholder='13.78' id='hurdles' type="decimal" value={hurdles} onChange={e => setHurdles(e.target.value)} />
-            <input placeholder='45.2' id='discus' type="decimal" value={discus} onChange={e => setDisc(e.target.value)} />
-            <input placeholder='4.20' id='poleVault' type="decimal" value={poleVault} onChange={e => setPole(e.target.value)} />
-            <input placeholder='56.20' id='javelin' type="decimal" value={javelin} onChange={e => setJav(e.target.value)} />
-            <input placeholder='4' id='minutes' type="decimal" value={minutes} onChange={e => setMin(e.target.value)} />
-            <input placeholder='28.2' id='seconds' type="decimal" value={seconds} onChange={e => setSec(e.target.value)} />
-            <button name='btnName' type="submit">Update</button>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
+  
   const ShowDecas = ({ decas }) => {
     const hundredPoints = () => {
       var number = 0;
